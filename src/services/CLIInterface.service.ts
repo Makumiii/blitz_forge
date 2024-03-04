a3ac9d32ed14978c6740e46316e1fabb94f2cd32
Makumi J <63513904+Makumiii@users.noreply.github.com>
2024-03-04 21:22:07 +0300

    add cli interface service

--- src/services/CLIInterface.service.ts
+++ src/services/CLIInterface.service.ts
@@ -1,9 +1,13 @@
-class CLIInterface{
-    constructor(){
-    }
-
-    public setup(){
-
-    }
-
-}
+// import {Separator, input} from '@inquirer/prompts';
+// import * as packageJson from 'package.json';
+// class CLIInterface{
+//     constructor(){
+//     }
+//
+//     public setup(){
+//         const version = packageJson.version;
+//         const welcomeBanner = `BLITZ FORGE ${version}`;
+//         console.log(welcomeBanner);
+//     }
+//
+// }
