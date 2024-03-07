0cf8e579dc9c358c9207c458418eff829f19ba18
Makumi J <63513904+Makumiii@users.noreply.github.com>
2024-03-07 21:57:11 +0300

    add logic to getTodos method in taskTracker clas

--- src/services/taskTracker.service.ts
+++ src/services/taskTracker.service.ts
@@ -1,17 +1,23 @@
 import * as fs from "fs";
+import * as path from "node:path";
 
 
 class taskTracker{
     public cwd:string;
-    public todoRegex:RegExp;
+    public commentSignatureRegex:RegExp;
     public supportedFiles:string[];
+    public store:string[];
+    public bulletsSignatureRegex:RegExp
     constructor(){
         this.cwd = process.cwd();
-        this.todoRegex = /^([/*])([->].*)+\1$/;
+        this.commentSignatureRegex = /^(\/\*)((->).*)+(\*\/)$/;
+        this.bulletsSignatureRegex = /^((->).*)+$/
         this.supportedFiles = ['.ts', '.tsx', '.js', '.jsx'];
+        this.store = [];
+
     }
 
-    public async readOperations(pathToFile:string, option?:{stream:boolean}):Promise<string | null | fs.ReadStream>{
+    static async readOperations(pathToFile:string, option?:{stream:boolean}):Promise<string | null | fs.ReadStream>{
         try{
             const streamData = option?.stream === true;
             if(!streamData){
@@ -28,14 +34,14 @@ class taskTracker{
         }
     }
 
-    public async writeOperations(path:string,data:string, options?:{stream:boolean}):Promise<void | fs.WriteStream>{
+    public async writeOperations(writePath:string,data:string, options?:{stream:boolean}):Promise<void | fs.WriteStream>{
         try{
             const streamable = options?.stream === true;
             if(!streamable){
-                await fs.promises.writeFile(path,  data, {encoding:"utf-8"});
+                await fs.promises.writeFile(writePath,  data, {encoding:"utf-8"});
                 return;
             }
-            return fs.createWriteStream(path,{encoding:"utf-8"});
+            return fs.createWriteStream(writePath,{encoding:"utf-8"});
 
 
         }
@@ -60,6 +66,51 @@ class taskTracker{
             const message = 'an error occurred while piping stream';
             console.error(message, err);
             return {done:false};
+        }
+
+    }
+    public async getTodos(projectRootPath:string):Promise<void>{
+        try{
+            const srcPath = path.resolve(projectRootPath, 'src');
+            const srcContents = await fs.promises.readdir(srcPath, {encoding:"utf-8"});
+
+            for(const content of srcContents){
+                const contentPath = path.resolve(srcPath, content);
+                const contentType = (await fs.promises.stat(contentPath)).isDirectory()
+                if(await new taskTracker().isSupportedFile(contentPath)){
+                    const fileContent = await taskTracker.readOperations(contentPath, {stream:false}) as string;
+                    const match = this.commentSignatureRegex.exec(fileContent);
+                    if(match !== null){
+                        const matchString = match[0];
+                    }
+
+
+
+                }
+
+
+            }
+
+        }
+        catch(err){
+            const message = 'an error occurred while getting todos from src files';
+            console.error(message, err);
+        }
+    }
+
+    private async isSupportedFile(filePath:string):Promise<boolean>{
+        try{
+            const file = await fs.promises.stat(filePath);
+            const isFile = file.isFile();
+            const extName = path.extname(filePath);
+            return this.supportedFiles.includes(extName) && isFile;
+
+        }
+        catch(err){
+            const message = 'an error occurred while checking if file is a supported file';
+            console.error(message, err);
+            return false
+
         }
 
     }
